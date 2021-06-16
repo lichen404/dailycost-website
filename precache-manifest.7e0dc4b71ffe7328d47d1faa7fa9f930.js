1d76eaf596cdcb7e01b7ed179bdedc2c3767b82f
lichen404 <lichen@genew.com>
2021-06-16 15:33:16 +0800

    deploy

---
+++ precache-manifest.7e0dc4b71ffe7328d47d1faa7fa9f930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05572a39967d8012bc732bda036a6cc6",
+    "url": "/dailycost-website/QRcode.png"
+  },
+  {
+    "revision": "f1fe0cc9b3d409cd3afa",
+    "url": "/dailycost-website/css/app.e119fb33.css"
+  },
+  {
+    "revision": "2c7ef77ab7a70ebb107bd1603f246cb3",
+    "url": "/dailycost-website/index.html"
+  },
+  {
+    "revision": "f1fe0cc9b3d409cd3afa",
+    "url": "/dailycost-website/js/app.7a4a94b5.js"
+  },
+  {
+    "revision": "3dd521fc808770703dc3",
+    "url": "/dailycost-website/js/chunk-vendors.30303a4c.js"
+  },
+  {
+    "revision": "0921b2935f6c6762530adc66ff7315cd",
+    "url": "/dailycost-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dailycost-website/robots.txt"
+  }
+]);
